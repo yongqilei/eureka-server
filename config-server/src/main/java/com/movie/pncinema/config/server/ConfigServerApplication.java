aac0c0c4ca4d420dd40c0180cd367c0a3141d523
leiyongqi <2540356035@qq.com>
2019-05-16 08:35:48 +0900

    配置中心工程提交

---
+++ config-server/src/main/java/com/movie/pncinema/config/server/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.movie.pncinema.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author leiyongqi
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
